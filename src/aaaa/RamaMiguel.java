19721ebe25d5b6f899a7bb203aacbe47c69fc0da
mserna <mserna@LAP0041.gruposiglo.local>
2024-05-14 06:03:34 -0500

    Jacobp, Clase Creada

---
+++ src/aaaa/RamaMiguel.java
@@ -0,0 +1,10 @@
+package aaaa;
+
+public class RamaMiguel {
+	public static void main(String[] args) {
+		System.out.println("hola");
+		System.out.println("Hola Jacobo, Soy Miguel Serna");
+		System.out.println("Hola Jacobo, Ya puedes unir lsa Ramas");
+	}
+
+}
