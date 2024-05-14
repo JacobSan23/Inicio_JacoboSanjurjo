6a7c12bd48f7fe0a2a8387b7f7a29e80ef14ce08
mserna <mserna@LAP0041.gruposiglo.local>
2024-05-14 06:01:27 -0500

    Rama actualizada

--- src/aaaa/jacob.java
+++ src/aaaa/jacob.java
@@ -1,9 +1,12 @@
 package aaaa;
 
+import javax.swing.plaf.basic.BasicSplitPaneUI.BasicHorizontalLayoutManager;
+
 public class jacob {
 	
 	public static void main(String[] args) {
 		System.out.println("hola");
 		System.out.println("Hola Jacobo, Soy Miguel Serna");
+		System.out.println("Hola Jacobo, Ya puedes unir lsa Ramas");
 	}
 }
